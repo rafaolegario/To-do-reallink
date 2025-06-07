6f1d8918e744b9c53b5460ccdf7fa9f455768de2
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-06-07 02:16:31 -0300

    feat: create tasks repository

---
+++ Backend/src/domain/application/To-do-list/repositories/tasks-repository.php
@@ -0,0 +1,11 @@
+<?php
+  require_once 'src/domain/entities/Task.php';
+
+  interface TasksRepository {
+    public function create(Task $task): void;
+    public function findById(int $id): ?Task;
+    public function getAll(): array;
+    public function update(Task $task, int $id): void;
+    public function delete(int $id): void;
+  }
+?>
