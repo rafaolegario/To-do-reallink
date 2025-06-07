3c4476a68208369ca0a85bc84062342c9ac09c53
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-06-07 02:17:37 -0300

    feat: edit task use case

---
+++ Backend/src/domain/application/To-do-list/use-cases/edit-task.php
@@ -0,0 +1,33 @@
+<?php
+  require_once 'src/domain/application/To-do-list/repositories/tasks-repository.php';
+  require_once 'src/domain/entities/Task.php';
+
+  class EditTaskUseCase {
+    private $taskRepository;
+
+    public function __construct(TasksRepository $taskRepository) {
+      $this->taskRepository = $taskRepository;
+    }
+
+    public function execute($id, $title, $description, $status) {
+      $task = $this->taskRepository->findById($id);
+
+      if(!$task){
+        throw new InvalidArgumentException("Task with id: $id not found.");
+      }
+
+      $task->setTitle($title);
+      $task->setDescription($description);
+      $task->setStatus($status);
+
+
+      $this->taskRepository->update($task, $id);
+
+      return [
+        'message' => 'Task edited with success',
+        'task' => $task, 
+      ];
+
+    }
+  }
+?>
