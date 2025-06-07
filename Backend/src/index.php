594d553669eed748f88648dcd4d8f54d05e3a347
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-06-07 02:19:42 -0300

    feat: create index

---
+++ Backend/src/index.php
@@ -0,0 +1,62 @@
+<?php
+header('Content-Type: application/json');
+require_once 'src/infrastructure/controllers/task-controller.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$controller = new TaskController();
+
+switch ($method) {
+  case 'GET':
+    if ($uri === '/tasks') {
+          $controller->getAll();
+    } elseif (preg_match('/^\/tasks\/(\d+)$/', $uri, $matches)) {
+          $id = $matches[1];
+          $controller->get($id);
+      } else {
+          http_response_code(404);
+          echo json_encode(['error' => 'Route not found']);
+      }
+    break;
+
+    case 'POST':
+        if ($uri === '/tasks') {
+            $data = json_decode(file_get_contents('php://input'), true);
+            $controller->create($data['title'] ?? '', $data['description'] ?? '');
+        } else {
+            http_response_code(404);
+            echo json_encode(['error' => 'Route not found']);
+        }
+        break;
+
+    case 'PUT':
+        if (preg_match('/^\/tasks\/(\d+)$/', $uri, $matches)) {
+            $id = $matches[1];
+            $data = json_decode(file_get_contents('php://input'), true);
+            $controller->edit(
+                $id,
+                $data['title'] ?? '',
+                $data['description'] ?? '',
+                $data['status'] ?? ''
+            );
+        } else {
+            http_response_code(404);
+            echo json_encode(['error' => 'Route not found']);
+        }
+        break;
+
+    case 'DELETE':
+        if (preg_match('/^\/tasks\/(\d+)$/', $uri, $matches)) {
+            $id = $matches[1];
+            $controller->delete($id);
+        } else {
+            http_response_code(404);
+            echo json_encode(['error' => 'Route not found']);
+        }
+        break;
+
+    default:
+        http_response_code(405);
+        echo json_encode(['error' => 'Method not allowed']);
+}
