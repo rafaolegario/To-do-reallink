ef82a6a3973331555957c8d507135cd75b7042f2
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-06-08 15:35:51 -0300

    removed files

--- Backend/src/infra/database/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS task_manager;
-USE task_manager;
-
-CREATE TABLE IF NOT EXISTS tasks (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  description TEXT,
-  status ENUM('pending', 'completed') DEFAULT 'pending'
-);
