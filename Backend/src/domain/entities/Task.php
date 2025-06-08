ce95e77bf66529a7f1ea05c0f9e2ff6c99428c09
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-06-08 15:35:40 -0300

    feat: add jsonSerialize

--- Backend/src/domain/entities/Task.php
+++ Backend/src/domain/entities/Task.php
@@ -1,39 +1,64 @@
 <?php
-  class Task {
-    private $title;
-    private $description;
-    private $status;
+class Task implements JsonSerializable {
+  private $id;
+  private $title;
+  private $description;
+  private $status;
 
-    public function __construct($id, $title, $description) {
-      $this->title = $title;
-      $this->description = $description;
-      $this->status = 'pending';
-    }
+  public function __construct($id, $title, $description) {
+    $this->id = $id;
+    $this->title = $title;
+    $this->description = $description;
+    $this->status = 'pending';
+  }
 
-    public function getTitle() {
-      return $this->title;
-    }
+  // Getters
+  public function getId() {
+    return $this->id;
+  }
 
-    public function getDescription() {
-      return $this->description;
-    }
+  public function getTitle() {
+    return $this->title;
+  }
 
-    public function getStatus() {
-      return $this->status;
-    }
+  public function getDescription() {
+    return $this->description;
+  }
 
-    public function setTitle($title) {
-      $this->title = $title;
-    }
-    public function setDescription($description) {
-      $this->description = $description;
-    }
-    public function setStatus($status) {
-      if ($status === 'pending' || $status === 'completed') {
-        $this->status = $status;
-      }else {
-        throw new InvalidArgumentException("Invalid status: $status");
-      }
+  public function getStatus() {
+    return $this->status;
+  }
+
+  // Setters
+  public function setId($id) {
+    $this->id = $id;
+  }
+
+  public function setTitle($title) {
+    $this->title = $title;
+  }
+
+  public function setDescription($description) {
+    $this->description = $description;
+  }
+
+  public function setStatus($status) {
+    if ($status === 'pending' || $status === 'completed') {
+      $this->status = $status;
+    } else {
+      throw new InvalidArgumentException("Invalid status: $status");
     }
   }
+
+  // Para json_encode funcionar
+  public function jsonSerialize(): mixed {
+    return [
+      'id' => $this->id,
+      'title' => $this->title,
+      'description' => $this->description,
+      'status' => $this->status,
+    ];
+}
+
+}
 ?>
