53d239af0d6fd70ca20b2b13a758725c501b16a1
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-06-09 18:13:55 -0300

    feat: add props

--- Frontend/to-do-list/src/components/ListTasks.tsx
+++ Frontend/to-do-list/src/components/ListTasks.tsx
@@ -1,36 +1,45 @@
 import { FaEye, FaPen, FaTrash } from "react-icons/fa";
 
 export type Task = {
-  id: number;
-  title: string;
-  description: string;
-  status: 'pending' | 'completed';
+  id: number
+  title: string
+  description: string
+  status: 'pending' | 'completed'
 };
 
-function ListTasks(props: { tasks: Task[], filter?: 'all' | 'completed' | 'pending' }) {
-  const { tasks, filter } = props;
+interface ListTasksProps {
+  tasks: Task[]
+  filter?: 'all' | 'completed' | 'pending'
+  saveTask?: (task: Task) => void
+}
 
-  const filteredTasks = tasks.filter(task => {
-    if (filter === 'all') return true;
-    return task.status === filter;
-  });
+function ListTasks(props: ListTasksProps) {
+
+  const filteredTasks = props.filter === 'all'
+    ? props.tasks
+    : props.tasks.filter(task => task.status === props.filter)
 
   return (
-    <div className="list-tasks overflow-auto">
+    <div className="  h-[85%] list-tasks overflow-auto mt-3">
       <ul className="bg-transparent p-4 rounded shadow-lg">
         {filteredTasks.map(task => (
           <li
             key={task.id}
-            className="bg-gray-800 p-2 mb-2 rounded shadow hover:shadow-lg transition-shadow flex items-center justify-between"
+            className={`bg-gray-800 p-2 mb-2 rounded shadow hover:shadow-lg transition-shadow flex items-center justify-between ${task.status === 'completed' ? 'line-through text-gray-500 bg-green-900' : 'text-white'} `}
           >
             <div className="flex items-center">
               <input
                 type="checkbox"
                 className="mr-2"
                 checked={task.status === 'completed'}
-                readOnly
+                onChange={() => {
+                  if (props.saveTask) {
+                    props.saveTask({ ...task, status: task.status === 'completed' ? 'pending' : 'completed' });
+                  }
+                }}
+               
               />
-              <h3 className="font-bold text-white">{task.title}</h3>
+              <h3 className="font-bold">{task.title}</h3>
             </div>
             <div className="flex items-center gap-4 text-white">
               <button className="cursor-pointer hover:text-[#007acc]"><FaEye size={18} /></button>
