0029b1710f90b7ea67937751412e01bdc9275b71
rafaolegario <rafaelbisofficel@gmail.com>
2025-06-09 22:34:37 -0300

    feat: implement add task

--- Frontend/to-do-list/src/components/AddTask.tsx
+++ Frontend/to-do-list/src/components/AddTask.tsx
@@ -1,13 +1,53 @@
-function AddTask(){
+import type { FormEvent } from "react";
+
+interface AddTaskProps {
+  OnAddTask?: (task: { title: string; description: string }) => void
+}
+
+function AddTask({ OnAddTask }: AddTaskProps) {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const form = e.currentTarget
+    const title = (form.elements.namedItem("title") as HTMLInputElement).value.trim()
+    const description = (form.elements.namedItem("description") as HTMLTextAreaElement).value.trim()
+
+    if (!title || !description) {
+      alert('O título e descrição não podem ser vazios')
+      return
+    }
+
+    OnAddTask?.({
+      title,
+      description
+    })
+    form.reset()
+  };
+
   return (
     <div className="add-task">
-      <form>
-        <input type="text" placeholder="Task Title" required />
-        <textarea placeholder="Task Description" required></textarea>
-        <button type="submit">Add Task</button>
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Título da tarefa"
+          required
+          className="p-2 rounded bg-gray-800 text-white border border-blue-500 outline-0"
+        />
+        <textarea
+          name="description"
+          placeholder="Descrição da tarefa"
+          required
+          className="p-2 rounded bg-gray-800 text-white resize-none border border-blue-500 outline-0"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 cursor-pointer"
+        >
+          Adicionar Tarefa
+        </button>
       </form>
     </div>
-  )
+  );
 }
 
 export default AddTask;
