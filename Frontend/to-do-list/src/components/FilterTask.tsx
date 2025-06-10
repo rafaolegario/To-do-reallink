8595bdefa837d6f863a6788fd28e5313995cd50c
rafaolegario <rafaelbisofficel@gmail.com>
2025-06-09 22:34:56 -0300

    refactor: change styles

--- Frontend/to-do-list/src/components/FilterTask.tsx
+++ Frontend/to-do-list/src/components/FilterTask.tsx
@@ -1,7 +1,7 @@
 function FilterTask({ onChange }: { onChange: (value: 'all' | 'completed' | 'pending') => void }) {
   return (
     <div className="filter-task w-full flex flex-col justify-center items-center">
-      <h2 className="text-[#d4d4d4] font-bold">Filtrar tarefas:</h2>
+      <h2 className="text-[#d4d4d4] font-bold mb-4 text-2xl">Filtrar tarefas:</h2>
       <select
         className="bg-gray-800 text-white p-2 rounded shadow-lg hover:shadow-xl transition-shadow w-full"
         onChange={(e) => onChange(e.target.value as 'all' | 'completed' | 'pending')}
