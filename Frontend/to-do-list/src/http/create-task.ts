db0b02bfa496df943e77ec1cbcc85c85c84b476c
rafaolegario <rafaelbisofficel@gmail.com>
2025-06-09 22:36:08 -0300

    feat: create task http

---
+++ Frontend/to-do-list/src/http/create-task.ts
@@ -0,0 +1,23 @@
+
+export async function createTask(task: {title: string, description: string}) {
+  const request = {
+    title: task.title,
+    description: task.description
+  }
+
+  const response = await fetch(`http://localhost:8000/tasks`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(request)
+
+  })
+
+  if(!response.ok){
+    throw new Error('Erro ao criar task')
+  }
+
+  const data = await response.json()
+  return data
+}
