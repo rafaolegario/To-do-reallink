2a6523c40c1606421893d62bc5e16feadc34668e
rafaolegario <rafaelbisofficel@gmail.com>
2025-06-09 22:33:54 -0300

    feat: add new submit methods

--- Frontend/to-do-list/src/App.tsx
+++ Frontend/to-do-list/src/App.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
-
 import AddTask from './components/AddTask'
 import ListTasks, { type Task } from './components/ListTasks'
 import FilterTask from './components/FilterTask'
 import SideBar from './components/SideBar'
 import { getAllTasks } from './http/get-all-tasks'
 import { updateTask } from './http/update-task'
-
+import { createTask } from './http/create-task'
+import { deleteTask } from './http/delete-task'
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([])
@@ -14,21 +14,33 @@ function App() {
   const [activeTab, setActiveTab] =  useState<'add' | 'filter'>('add')
   const [filter, setFilter] = useState<'all' | 'completed' | 'pending'>('all');
 
-  useEffect(() => {
-  const fetchTasks = async () => {
+  async function fetchTasks() {
     const data = await getAllTasks()
     if (data) setTasks(data)  
   }
-    fetchTasks()
-  }, [])
 
-  async function saveTask(task: Task) {
+  useEffect(() => {fetchTasks()}, [])
+
+  async function OnAddTask(task: {title: string, description: string}) {
+    const newTask = await createTask(task)
+    if (newTask) {
+      setTasks(prev => [...prev, newTask])
+    }
+  }
+
+  async function OnSaveTask(task: Task) {
+    await updateTask(task)
     setTasks(prevTasks =>
     prevTasks.map(t => (t.id === task.id ? task : t))
     )
-    await updateTask(task)
   }
 
+  async function OnDeleteTask(taskId: number) {
+    setTasks(prevTasks =>
+    prevTasks.filter(t => t.id !== taskId)
+    )
+    await deleteTask(taskId)
+  }
 
   return (
     <div className='w-screen h-screen bg-[#1e1e1e] flex flex-col md:flex-row items-center justify-center p-2 gap-4'>
@@ -38,15 +50,15 @@ function App() {
         
         {activeTab === 'add' && (
           <>
-            <AddTask />
-            <ListTasks tasks={tasks} filter='all' saveTask={saveTask} />
+            <AddTask OnAddTask={OnAddTask} />
+            <ListTasks tasks={tasks} filter='all' OnSaveTask={OnSaveTask} OnDeleteTask={OnDeleteTask} />
           </>
         )}
 
         {activeTab === 'filter' && (
           <div className='text-[#d4d4d4]'>
             <FilterTask onChange={setFilter} />
-            <ListTasks tasks={tasks} filter={filter} saveTask={saveTask} />
+            <ListTasks tasks={tasks} filter={filter} OnSaveTask={OnSaveTask} OnDeleteTask={OnDeleteTask} />
           </div>
         )}
       </div>
