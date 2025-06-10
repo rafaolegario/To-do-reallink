4cf2e29e60986140dd2291a92dd82494559d55bd
rafaolegario <rafaelbisofficel@gmail.com>
2025-06-09 22:35:22 -0300

    refactor: change method names

--- Frontend/to-do-list/src/components/ListTasks.tsx
+++ Frontend/to-do-list/src/components/ListTasks.tsx
@@ -10,7 +10,8 @@ export type Task = {
 interface ListTasksProps {
   tasks: Task[]
   filter?: 'all' | 'completed' | 'pending'
-  saveTask?: (task: Task) => void
+  OnSaveTask?: (task: Task) => void
+  OnDeleteTask?: (taskId: number) => void
 }
 
 function ListTasks(props: ListTasksProps) {
@@ -21,30 +22,30 @@ function ListTasks(props: ListTasksProps) {
 
   return (
     <div className="  h-[85%] list-tasks overflow-auto mt-3">
-      <ul className="bg-transparent p-4 rounded shadow-lg">
+      <ul className="bg-transparent p-3 ">
         {filteredTasks.map(task => (
           <li
             key={task.id}
-            className={`bg-gray-800 p-2 mb-2 rounded shadow hover:shadow-lg transition-shadow flex items-center justify-between ${task.status === 'completed' ? 'line-through text-gray-500 bg-green-900' : 'text-white'} `}
+            className={`bg-gray-800 p-3 mb-2 rounded shadow hover:shadow-lg transition-shadow flex items-center justify-between ${task.status === 'completed' ? 'line-through text-gray-500 bg-green-900' : 'text-white'} `}
           >
             <div className="flex items-center">
               <input
                 type="checkbox"
-                className="mr-2"
+                className="mr-2 w-4 h-4 accent-[#007acc] cursor-pointer"
                 checked={task.status === 'completed'}
                 onChange={() => {
-                  if (props.saveTask) {
-                    props.saveTask({ ...task, status: task.status === 'completed' ? 'pending' : 'completed' });
+                  if (props.OnSaveTask) {
+                    props.OnSaveTask({ ...task, status: task.status === 'completed' ? 'pending' : 'completed' });
                   }
                 }}
                
               />
-              <h3 className="font-bold">{task.title}</h3>
+              <h3 className="font-bold text-lg truncate max-w-xs">{task.title}</h3>
             </div>
             <div className="flex items-center gap-4 text-white">
               <button className="cursor-pointer hover:text-[#007acc]"><FaEye size={18} /></button>
               <button className="cursor-pointer hover:text-[#007acc]"><FaPen size={18} /></button>
-              <button className="cursor-pointer hover:text-[#007acc]"><FaTrash size={18} /></button>
+              <button className="cursor-pointer hover:text-[#007acc]" onClick={() => props.OnDeleteTask?.(task.id)}><FaTrash size={18} /></button>
             </div>
           </li>
         ))}
