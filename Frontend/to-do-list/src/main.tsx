792a5f9491e3788078325706a361869ff50f4f92
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-06-10 11:45:25 -0300

    feat: add new path

--- Frontend/to-do-list/src/main.tsx
+++ Frontend/to-do-list/src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ViewTask from './components/ViewTask.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element:<App/>
+  },
+  {
+    path: '/task',
+    element: <ViewTask />
   }
 ])
 
